f2cdbee93d585240227948e894f1adb3a48d0a87
irenetrecu <67189040+irenetrecu03@users.noreply.github.com>
2024-11-04 15:56:00 +0100

    Disable going back to register

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,8 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false, gestureEnabled: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="register" options={{ headerShown: false }} />
       </Stack>
     </AuthProvider>
